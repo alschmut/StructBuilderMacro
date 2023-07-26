e4e07f6142f1e3f9eb0bae0a4e4cdcfe08dfccc5
Alexander Schmutz <alexander.schmutz@maibornwolff.de>
2023-07-26 21:00:38 +0200

    Simplify package.swift

--- Package.swift
+++ Package.swift
@@ -8,24 +8,13 @@ let package = Package(
     name: "StructBuilder",
     platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "StructBuilder",
-            targets: ["StructBuilder"]
-        ),
-        .executable(
-            name: "StructBuilderClient",
-            targets: ["StructBuilderClient"]
-        ),
+        .library(name: "StructBuilder", targets: ["StructBuilder"]),
+        .executable(name: "StructBuilderClient", targets: ["StructBuilderClient"]),
     ],
     dependencies: [
-        // Depend on the latest Swift 5.9 prerelease of SwiftSyntax
         .package(url: "https://github.com/apple/swift-syntax.git", from: "509.0.0-swift-5.9-DEVELOPMENT-SNAPSHOT-2023-07-10-a"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        // Macro implementation that performs the source transformation of a macro.
         .macro(
             name: "StructBuilderMacro",
             dependencies: [
@@ -33,14 +22,8 @@ let package = Package(
                 .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
             ]
         ),
-
-        // Library that exposes a macro as part of its API, which is used in client programs.
         .target(name: "StructBuilder", dependencies: ["StructBuilderMacro"]),
-
-        // A client of the library, which is able to use the macro in its own code.
         .executableTarget(name: "StructBuilderClient", dependencies: ["StructBuilder"]),
-
-        // A test target used to develop the macro implementation.
         .testTarget(
             name: "StructBuilderMacroTests",
             dependencies: [
