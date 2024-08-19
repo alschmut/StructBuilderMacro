7a06c9786385314b1cafdb0089d45734d1aceb38
Alexander Schmutz <aschmutz1@web.de>
2024-08-19 17:56:00 +0200

    Remove redundant cast

--- Sources/BuildableMacro/StructAndClass/Helper/ExtractInitializerMembers.swift
+++ Sources/BuildableMacro/StructAndClass/Helper/ExtractInitializerMembers.swift
@@ -8,10 +8,8 @@
 import SwiftSyntax
 
 func extractInitializerMembers(from initialiserDecl: InitializerDeclSyntax) -> [StructMember] {
-    initialiserDecl.signature.parameterClause.parameters.compactMap {
-        guard let functionParameter = $0.as(FunctionParameterSyntax.self) else { return nil }
-        let identifier = functionParameter.firstName.trimmed
-        let type = functionParameter.type
-        return StructMember(identifier: identifier, type: type)
-    }
+    initialiserDecl.signature.parameterClause.parameters
+        .compactMap {
+            StructMember(identifier: $0.firstName.trimmed, type: $0.type)
+        }
 }
